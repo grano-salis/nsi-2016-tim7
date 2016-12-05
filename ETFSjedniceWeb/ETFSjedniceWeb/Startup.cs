e60337bbc60e93ececce68766f8493afd78ffc1b
SaudinAdemovic <sademovic1@etf.unsa.ba>
2016-12-05 13:37:18 +0100

    Projekat sa .net loginom

---
+++ ETFSjedniceWeb/ETFSjedniceWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETFSjedniceWeb.Startup))]
+namespace ETFSjedniceWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
